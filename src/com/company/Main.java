5fc730e3a0b8e3d80db23da3bf62581521d3f593
BINT <sitthichai.srithi@gmail.com>
2018-07-27 23:35:22 +0700

    27/07/2018

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,10 @@ package com.company;
 import com.sun.istack.internal.Nullable;
 
 
-public class Main {
+//public class Main {
 
 //// #1
-    //    public static void main(String[] args) {
+//    public static void main(String[] args) {
 //        int Basic = Basic1();
 //
 //        System.out.print(Basic);
@@ -28,50 +28,111 @@ public class Main {
 //        return a;
 //    }
 
-    //// #2 Overload
-    public static void sayHello() {
+//// #2 Overload
+//    public static void sayHello() {
+//
+//        System.out.println("Hello, JAVA");
+//
+//    }
+//
+//    public static void sayHello(String name) {
+//
+//        System.out.println("Hello, " + name.toString());
+//
+//    }
+//
+//    public static void main(String args[]) {
+//
+//        sayHello();
+//        sayHello("amplysoft");
+//
+//    }
+//
+//
+//}
 
-        System.out.println("Hello, JAVA");
+///// #3  Interface Class Java การสืบทอดที่สามาีถมีแค่ชื่อ เมดธอด
+//interface Interface {
+//    void sayHello();
+//
+//    void sayWebsite();
+//}
+//
+//class Sample implements Interface {
+//    public void sayHello() {
+//        System.out.println("amplysoft");
+//    }
+//
+//    public void sayWebsite() {
+//        System.out.println("www.amplysoft.com");
+//    }
+//}
+//
+//class Program {
+//    public static void main(String args[]) {
+//        Sample s = new Sample();
+//        s.sayHello();
+//        s.sayWebsite();
+//    }
+//}
 
+
+//// #4 Abstract class การสืบทอดที่สามาีถมีแค่ชื่อหรือการทำงานใน เมดธอด
+//abstract class Abstract {
+//    public void sayHello(){
+//        System.out.println( "amplysoft" );
+//    }
+//    abstract void sayWebsite();
+//}
+//
+//class Sample extends Abstract  {
+//    public void sayWebsite(){
+//        System.out.println("www.amplysoft.com");
+//    }
+//}
+//
+//class Program {
+//    public static void main(String args[]){
+//        Sample s = new Sample();
+//        s.sayHello();
+//        s.sayWebsite();
+//    }
+//}
+
+
+/// #5 Inheritance การสืบทอด
+
+//class Inheritance {
+//    public static void getName() {
+//        System.out.println("amplysoft");
+//    }
+//
+//    public static void getWebSite() {
+//        System.out.println("www.amplysoft.com");
+//    }
+//}
+//
+//class Child extends Inheritance {
+//    public static void main(String args[]) {
+//        getName();
+//        getWebSite();
+//    }
+//}
+
+///#6 Override Methodd การสรา้งชื่อ  method  เดิม แต่การทำงานใหม่  ในคลาสลูก
+
+class Override {
+    public static void getWebSite(String name) {
+        System.out.println(name);
     }
+}
 
-    public static void sayHello(String name) {
-
-        System.out.println("Hello, " + name.toString());
-
+class Program extends Override {
+    public static void getWebSite(String name) {
+        System.out.println("124" + name);
     }
 
     public static void main(String args[]) {
-
-        sayHello();
-        sayHello("amplysoft");
-
-    }
-
-
-}
-
-///// #3  Interface Class Java
-interface Interface {
-    void sayHello();
-
-    void sayWebsite();
-}
-
-class Sample implements Interface {
-    public void sayHello() {
-        System.out.println("amplysoft");
-    }
-
-    public void sayWebsite() {
-        System.out.println("www.amplysoft.com");
-    }
-}
-
-class Program {
-    public static void main(String args[]) {
-        Sample s = new Sample();
-        s.sayHello();
-        s.sayWebsite();
+        getWebSite("www.amplysoft.com");
     }
 }
